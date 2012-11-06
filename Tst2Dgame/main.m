bfb7f4978b0fbb558d756532ab5e2d2af86bcadd
Maxim Khatskevich <maxim.khatskevich@gmail.com>
2012-11-06 12:38:56 +0400

    Initial Commit

---
+++ Tst2Dgame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Tst2Dgame
+//
+//  Created by Maxim Khatskevich on 11/6/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
